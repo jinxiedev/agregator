6611516d4752074b9dcd29b317521d25b2981b54
Jinxie Lunareth <yndazhangg@gmail.com>
2025-09-14 21:27:27 +0700

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
@@ -40,7 +40,7 @@ func handleAIRequest(w http.ResponseWriter, r *http.Request) {
 	
 	// Parse request body
 	var req AIRequest
-	body, err := ioutil.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		sendErrorResponse(w, "Failed to read request body", err)
 		return
